75e2118411bd85b6a92a823a7e8f28923286e6c4
abhaychauhan8802 <abhaychauhan8802@gmail.com>
2024-09-08 15:37:10 +0530

    improve folder structure

--- src/components/SearchLogo.jsx
+++ src/components/SearchLogo.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const SearchLogo = () => {
   return (
-    <div className="text-center">
-      <h1 className="font-serif text-8xl font-bold text-foreground ">Search</h1>
+    <div className="text-center select-none">
+      <h1 className="font-serif text-8xl font-bold text-foreground">Search</h1>
     </div>
   );
 };
